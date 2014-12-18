fcf3b4bacd7dba984a4f6a138f2053bac8f4e81c
Charlike Mike Reagent <mamemto_100@mail.bg>
2014-12-18 02:44:08 +0200

    commit from .bashrc

---
+++ berta.js
@@ -0,0 +1,51 @@
+//TODO
+
+function berta(template, data, opts) {
+  opts = opts || {};
+  opts.open = '{{';
+  opts.close = '}}'
+  var pattern = new RegExp('{{\\s?([^\\s]+)\\s?}}', 'g');
+
+  return template.replace(pattern, function(input, match) {
+    var path = match.split('.');
+    var len = path.length;
+    var cache = data;
+    var i = 0;
+
+    while (i < len){
+      cache = cache[path[i]];
+
+      if (!cache) {
+        return false;
+      }
+
+      if (i === len - 1) {
+        if (typeof cache === 'function') {
+          var fn = cache;
+          var str = fn.toString();
+          console.log(str)
+          return cache.call(data)
+        }
+        return cache;
+      }
+      i++;
+    }
+  });
+};
+
+var data = {
+  user: {
+    edit: 'Wikipedia',
+    name: {
+      age: 21,
+      calc: function() {
+        return this.user.name.age + 4;
+      }
+    }
+  },
+  nick: 'Charlike',
+}
+console.log(
+  berta('created by New {{ nick }} {{user.edit}} Campaigns, the {{ user.name.age }} behind and {{user.name.calc}}.',
+   data)
+  )
