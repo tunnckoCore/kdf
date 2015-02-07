80c308bde6e14b0083c3cc45d7511e707a4c2432
tunnckoCore <mameto_100@mail.bg>
2015-02-07 06:44:51 +0200

    fix mocha / dev deps

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,21 @@ var camelCase = require('camelcase');
 var mustache = require('gulp-mustache');
 var meow = require('meow');
 
-var opts = {
-  pkgName: 'hex-color-regex',
-  pkgDesc: 'Regular expression (regex) for matching hex color values from string.',
+var context = {
+  pkgName: 'gitfork',
+  pkgDesc: 'Fork a Github repo with node.js (programmatically) with just \`user/repo\` pattern or through [gitfork-cli][gitfork-cli] package for CLI usage.',
   pkgVers: '0.0.0',
-  pkgOrgs: 'regexps',
+  pkgOrgs: 'tunnckoCore',
   pkgDate: dateformat('longDate')
 };
-opts.pkgSens = camelCase(opts.pkgName)
+context.pkgSens = camelCase(context.pkgName)
 
 
 gulp.task('default', function defaultFn() {
   gulp.src(['./templates/*.', './templates/.*', './templates/*'])
-    .pipe(mustache(opts))
-    .pipe(gulp.dest(path.join('../', opts.pkgName)));
-    console.log('YOU GENERATE PROJECT:', path.join('../', opts.pkgName));
+    .pipe(mustache(context))
+    .pipe(gulp.dest(path.join('../', context.pkgName)));
+    console.log('YOU GENERATE PROJECT:', path.join('../', context.pkgName));
 });
 
 gulp.start()
